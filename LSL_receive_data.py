d5323e34e8f26d9b56179836d9c4a6efe85e1fa4
gretatu <35527169+gretatu@users.noreply.github.com>
2018-09-12 15:47:29 +0200

    Small edits LSL

--- LSL_receive_data.py
+++ LSL_receive_data.py
@@ -18,8 +18,9 @@ print("Number of streams found: %s " % len(streams))
 # Create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
 
-sample, timestamp = inlet.pull_sample()
+sample, timestamp = inlet.pull_sample() # pull_sample() gives the most recent sample of a stream
 
+# Checks
 print(sample)
 print(len(sample))
 print(timestamp)
@@ -27,14 +28,11 @@ print(timestamp)
 # Can also read time series from LSL in chunks
 # chunk, timestamps = inlet.pull_chunk()
 
-
 #while True:
     # get a new sample 
-#    sample, timestamp = inlet.pull_sample()
+    # sample, timestamp = inlet.pull_sample()
+    # print("Timestamp: \t %0.5f\n Sample: \n %s\n\n" %(timestamp,   sample))
 #print(timestamp, sample)
 
-# If I want to check the most recent sample of a stream, try pull_sample()
-
 # Implement local_clock()
-
 # LSL time stamps the data by push_sample() or to call the local_clock() function to read out the LSL clock, and then pass that in
